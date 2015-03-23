1a22d10018cba1e47109a21f61de61cc5d60e39d
David Rysdam <drysdam@ll.mit.edu>
2015-03-23 10:07:43 -0400

    use a figure so I can resize it once (can't figure out how from the code)

--- analyze_gears.m
+++ analyze_gears.m
@@ -1,5 +1,7 @@
 function [] = analyze_gears ( coeffs, f, oversamplerate, cycles )
   [xanalog yanalog xdigital ydigital] = crank_gears( coeffs, f, oversamplerate, cycles );
+  figure(1);
   plot(xanalog, yanalog, 'b', 'linewidth', 2); hold on;
-  plot(xdigital, ydigital, 'b o', 'markersize', 8, 'markerfacecolor', 'blue'); hold off;
+  %% during analyze, we just crank and pause
+  plot(xdigital, ydigital, 'b o', 'markersize', 3, 'markerfacecolor', 'blue'); hold off;
 endfunction
--- synthesize_gears.m
+++ synthesize_gears.m
@@ -1,5 +1,7 @@
 function [] = synthesize_gears ( coeffs, f, oversamplerate, cycles )
   [xanalog yanalog xdigital ydigital] = crank_gears( coeffs, f, oversamplerate, cycles );
-  plot(xanalog, yanalog, 'b', 'linewidth', 2); hold on;
-  plot(xdigital, ydigital, 'b o', 'markersize', 8, 'markerfacecolor', 'blue'); hold off;
+  figure(1);
+  plot(xanalog, yanalog, 'b', 'linewidth', 2);
+  %% during synthesize, we just crank
+  %% plot(xdigital, ydigital, 'b o', 'markersize', 8, 'markerfacecolor', 'blue'); hold off;
 endfunction
