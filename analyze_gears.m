25bc8eb669ef013df530498a98313912b77fbe50
David Rysdam <drysdam@ll.mit.edu>
2015-03-23 09:40:01 -0400

    analyze is exactly synthesize, but not sure that's right

--- analyze_gears.m
+++ analyze_gears.m
@@ -1,4 +1,5 @@
-function [ ] = analyze_gears ( samples, f )
-  [analog digital] = crank_gears(samples, f, length(samples), 1);
-  plot(analog);
+function [] = analyze_gears ( coeffs, f, oversamplerate, cycles )
+  [xanalog yanalog xdigital ydigital] = crank_gears( coeffs, f, oversamplerate, cycles );
+  plot(xanalog, yanalog, 'b', 'linewidth', 2); hold on;
+  plot(xdigital, ydigital, 'b o', 'markersize', 8, 'markerfacecolor', 'blue'); hold off;
 endfunction
