00ae6a0b399414c8a02b046e7394284af3e8f385
David Rysdam <drysdam@ll.mit.edu>
2015-03-23 09:16:27 -0400

    synthesize now making "analog" and "digital" overlay plots, lined up correctly

--- synthesize_gears.m
+++ synthesize_gears.m
@@ -1,4 +1,6 @@
-function [] = synthesize_gears ( coeffs, f, samplecount, cycles )
-  [analog digital] = crank_gears( coeffs, f, samplecount, cycles );
-  plot(analog);
+function [] = synthesize_gears ( coeffs, f, oversamplerate, cycles )
+  N = length(coeffs);
+  [xanalog yanalog xdigital ydigital] = crank_gears( coeffs, f, oversamplerate, cycles );
+  plot(xanalog, yanalog, 'b', 'linewidth', 2); hold on;
+  plot(xdigital, ydigital, 'b o', 'markersize', 8, 'markerfacecolor', 'blue'); hold off;
 endfunction
